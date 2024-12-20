763763b64b0dd8808729bf27ef8f170017f6abea
Ankur Patel <55826895+Ankur-Dhru@users.noreply.github.com>
2024-12-20 17:13:30 +0530

    footer content

--- src/pages/Navigation/Footer.js
+++ src/pages/Navigation/Footer.js
@@ -1,26 +1,55 @@
 import React, {Fragment} from "react";
 import {connect} from "react-redux";
 import {device} from "../../lib/static";
+import {getCompanyDetails} from "../../lib/functions";
 
 const Index = () => {
 
+    let {
+        location
+    } = getCompanyDetails();
+
+
+    if(!Boolean(device?.locationid)){
+        return null
+    }
+    if(!Boolean(location[device?.locationid]?.data)){
+        return null
+    }
+
+    const {footercontent} = location[device?.locationid]?.data
 
         return (
             <Fragment>
-                 <section className="">
-                    <footer className="footer footer--s3 footer--color-dark"  style={{marginBottom:'150px'}}>
+
+                <section className="">
+                    <div className="container">
+
+                        <div className="row">
+                            <div className="col-12 text-center">
+                                <pre>
+                                    {atob(footercontent)}
+                                </pre>
+                            </div>
+                        </div>
+
+                    </div>
+                </section>
+
+                <section className="">
+                    <footer className="footer footer--s3 footer--color-dark" style={{marginBottom: '150px'}}>
                         <div className="p-5">
                             <div className="container">
 
                                 <div className="row">
                                     <div className="col-12 text-center">
-                                        <div>
-                                            <label className="power-by" ><label onClick={()=>{
+                                    <div>
+                                            <label className="power-by"><label onClick={() => {
                                                 window.open(`https://www.dhru.com/?fromid=${device.workspace}`, '_blank');
                                             }}>Powered by Dhru ERP </label></label>
                                         </div>
 
-                                      </div>
+                                    </div>
                                 </div>
 
                             </div>
